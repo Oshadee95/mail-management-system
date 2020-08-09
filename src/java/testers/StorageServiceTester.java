1e337131b75e8e8cc59b2c2496a8c9f12b035424
redhawk96 <uditha1003@gmail.com>
2020-08-09 14:07:12 +0530

    StorageServices implemented

---
+++ src/java/testers/StorageServiceTester.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testers;
+
+import models.Storage;
+import services.StorageService;
+
+/**
+ *
+ * @author RED-HAWK
+ */
+public class StorageServiceTester {
+    
+    
+    public static void main(String args[]){
+        
+//        Storage s = new Storage();
+//        s.setFolderName("test");
+//        s.setFileName("testimg.jpg");
+//        
+//        try {
+//            System.out.println(StorageService.add(s));
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+    }
+}
